02d16bbe9ba24ff0ded69efd9c6fd2a6c007d143
yaniragl <yanira_galeas@hotmail.com>
2017-10-25 21:08:36 -0300

    Esta listo mi trabajo

---
+++ src/app1yanira/App1yanira.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1yanira;
+
+/**
+ *
+ * @author ceduc
+ */
+public class App1yanira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int numero;//Declaracion variable
+        int numero5;
+        numero=30;
+        numero=5;
+        
+        System.out.println("suma de numero y numero5 +(numero + numero5");
+        System.out.println("resta de numero y numer5 -(numero - numero5");
+        System.out.println("multiplicacion de numero y numero5 *(numero * numero5");
+        System.out.println("division de numero y numero5 /(numero/numero5");
+        System.out.println("resta del numero y numero5 %(numero%numer5");
+        
+        
+            
+        }
+            
+        }
+   
+    
+       
+        
+        
+        
+    
+    
+
